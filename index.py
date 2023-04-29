f2ea02d350bed1fb0a10251b9881c5da565a49f6
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-04-29 19:14:24 +0800

    initial

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from stockfish import Stockfish
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    stockfish = Stockfish("/usr/local/bin/stockfish")
+    stockfish.set_position(["e2e4", "e7e6"])
+    return stockfish.get_best_move()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
